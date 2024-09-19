210e6cc5160d8e23f158bdedf6094b67617f2112
nezz <42505844+nezztorlpz@users.noreply.github.com>
2024-09-19 03:03:04 -0600

    menuFix

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,12 +1,15 @@
-const toggleBtn = document.querySelector('.toggle_btn')
-const toggleBtnIcon = document.querySelector('.toggle_btn i')
-const dropDownMenu = document.querySelector('.dropdown_menu')
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.querySelector('.toggle_btn');
+    const toggleBtnIcon = document.querySelector('.toggle_btn i');
+    const dropDownMenu = document.querySelector('.dropdown_menu');
 
-toggleBtn.onclick = function(){
-    dropDownMenu.classList.toggle('open')
-    const isOpen = dropDownMenu.classList.contains('open')
+    toggleBtn.onclick = function() {
+        dropDownMenu.classList.toggle('open');
+        const isOpen = dropDownMenu.classList.contains('open');
 
-    toggleBtnIcon.classList = isOpen
-    ? 'fa-solid fa-xmark'
-    : 'fa-solid fa-bars'
-}
+        // Cambia la clase del ícono dependiendo del estado del menú
+        toggleBtnIcon.classList = isOpen
+            ? 'bi bi-x'  // Clase del ícono 'x' de Bootstrap Icons cuando el menú está abierto
+            : 'bi bi-list'; // Clase del ícono de hamburguesa cuando está cerrado
+    }
+});
